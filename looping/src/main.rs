3542e6f16f94668195860efd287b0de0865d3b06
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-09 00:39:26 +0100

    looping

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,6 +1,5 @@
 use std::io;
-// use std::io::prelude::*;
-// use std::fs::File;
+
 
 fn main() {
     let mut strr = String::new();
@@ -13,15 +12,17 @@ fn main() {
     let mut i = 0;
     loop {
         i += 1;
+         if str2.trim() == strr.trim() {
+            println!("Number of trials: {}", i);
+            break;
+        }
+        
         strr.clear();
         println!(
             "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
         );
         io::stdin().read_line(&mut strr).expect("Failed to read line");
-        if str2.trim() == strr.trim() {
-            println!("Number of trials: {}", i);
-            break;
-        }
+       
         
     }
 }
