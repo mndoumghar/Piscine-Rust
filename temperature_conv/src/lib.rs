a1b1888260ca17235c3f5e3ac4712503a32c4d58
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-08 23:07:41 +0100

    tepmerature_conve

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,13 +1,7 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    let x: f64 = 5.0;
-    let y: f64 = 9.0;
-    let z: f64 = 32.0;
-     (f - z) * x/y
+    (f - 32.0) * 5.0 / 9.0
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    let x: f64 = 5.0;
-    let y: f64 = 9.0;
-    let z: f64 = 32.0;
-     (c * y/x) + z 
+    c * 9.0 / 5.0 + 32.0
 }
