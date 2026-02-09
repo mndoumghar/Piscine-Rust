70e268067f0c2ca5a2356122d0cecf09ab1edf56
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-09 04:48:16 +0100

    Matrix ...

---
+++ matrix_transposition/src/lib.rs
@@ -0,0 +1,8 @@
+use std::fmt;
+#[derive(Debug)]
+pub struct Matrix(pub(i32, i32),pub (i32, i32));
+
+pub fn transpose(matrix: Matrix) -> Matrix {
+    Matrix(
+        (matrix.0.0, matrix.1.0), (matrix.0.1, matrix.1.1))
+}
