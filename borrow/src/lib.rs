3da05537cf0f74f8cf3ced3302c5cd1fdc00de37
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-09 19:44:05 +0100

    borrow

---
+++ borrow/src/lib.rs
@@ -0,0 +1,4 @@
+pub fn str_len(s: &str ) -> usize {
+    let mut ss  : Vec<char> = s.chars().collect();
+     ss.len()
+}
--- variables/fibonacci2/src/lib.rs
+++ variables/fibonacci2/src/lib.rs
@@ -1,8 +1,6 @@
 pub fn fibonacci(n: u32) -> u32 {
-    if n==0 {
-        return 0
-    }
-    if n == 1 || n== 2 {
+  
+    if n == 1 || n == 0{
        return  1
     }
 
