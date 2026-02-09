2cb5ec5874369ee5da17d4c715d1b16a70e46777
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-09 01:54:45 +0100

    groceries

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -16,13 +16,13 @@ fn main() {
             println!("Number of trials: {}", i);
             break;
         }
-        
+
         strr.clear();
         println!(
             "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
         );
+        
         io::stdin().read_line(&mut strr).expect("Failed to read line");
        
-        
     }
 }
