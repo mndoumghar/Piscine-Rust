e52cfa0a4d4f5cbed186e492ee1b0b145520a811
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-10 22:35:24 +0100

    copy exp ln

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -1,7 +1,5 @@
-
 pub fn nbr_function(c: i32) -> (i32, f64, f64) {
-    (c,(c as f64).exp(), ( c as f64).ln())
-    
+    (c, (c as f64).abs().exp(), (c as f64).abs().ln())
 }
 
 pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
@@ -9,25 +7,39 @@ pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
     let mut vec2 = Vec::new();
     for i in b {
         vec1.push(i);
-        vec2.push((i as f64).exp());
+        vec2.push((i as f64).abs().exp());
     }
-    (vec1, vec2)    
-
+    (vec1, vec2)
 }
-   
+
 pub fn str_function(a: String) -> (String, String) {
     let mut str1 = String::new();
     let mut str2 = String::new();
-    let mut length : i32 = 0;
-    for i in a.split(" ") {
-        if length != a.len() as i32 {
-        str1.push(' ');
-        str2.push(' ');        }  
-        str1.push_str(i);
-        str2.push_str(&((i.parse::<f64>().expect("errrrr") as f64).exp().to_string())); 
-        length+=1;     
-    }
-    
-    (str1, str2)
+    let  count: i32 = a
+    .split(' ')
+    .count()
+    .try_into()
+    .expect("errrrr");
+        let mut c = 0;
 
+    for i in a.split(" ") {
+         c = c +1;
+        str1.push_str(i);
+        str2.push_str(
+            &((i.parse::<f64>().expect("errrrr") as f64)
+                .abs()
+                .exp()
+                .to_string()),
+        );
+
+        if count > c  {
+            str1.push(' ');
+            str2.push(' ');
+
+        }  
+           
+        
+    }
+
+    (str1, str2)
 }
