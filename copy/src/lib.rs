d63534a71e48d90d4b4a048e69723968e00c29da
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-10 22:43:16 +0100

    copy exp ln

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -2,15 +2,7 @@ pub fn nbr_function(c: i32) -> (i32, f64, f64) {
     (c, (c as f64).abs().exp(), (c as f64).abs().ln())
 }
 
-pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
-    let mut vec1 = Vec::new();
-    let mut vec2 = Vec::new();
-    for i in b {
-        vec1.push(i);
-        vec2.push((i as f64).abs().exp());
-    }
-    (vec1, vec2)
-}
+
 
 pub fn str_function(a: String) -> (String, String) {
     let mut str1 = String::new();
@@ -31,15 +23,22 @@ pub fn str_function(a: String) -> (String, String) {
                 .exp()
                 .to_string()),
         );
-
         if count > c  {
             str1.push(' ');
             str2.push(' ');
 
-        }  
-           
-        
+        }       
     }
 
     (str1, str2)
+}
+pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
+
+    let mut vec1 = Vec::new();
+    let mut vec2 = Vec::new();
+    for i in b {
+        vec1.push(i);
+        vec2.push((i as f64).abs().ln());
+    }
+    (vec1, vec2)
 }
