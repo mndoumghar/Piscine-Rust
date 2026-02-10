aa54db39d7bae995e1c320b666401ce313196d92
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-10 17:11:08 +0100

    name initials

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -8,10 +8,13 @@ pub fn initials(names: Vec<&str>) -> Vec<String> {
             let mut count: i32 = 0;
 
             for c in word.split_whitespace() {
-                if count>0 {
-                    res.push_str(". ");
-                }
-                res.push(c.chars().next().expect("hiy li bghat"));    
+                 if count>0 {
+                    res.push_str(" ");
+                }  
+              
+                res.push(c.chars().next().expect("hiy li bghat"));
+                res.push('.');
+                
                 count+= 1;
             }
             res
