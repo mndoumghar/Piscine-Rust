072128972a6fa5df8da5d8d9c174a075b056c8f6
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-11 22:46:31 +0100

    random card_deck

---
+++ card_deck/src/lib.rs
@@ -0,0 +1,64 @@
+use rand::prelude::*;
+#[derive(Debug, PartialEq)]
+pub enum Suit {
+    Heart,
+    Diamond,
+    Spade,
+    Club,
+}
+
+#[derive(Debug, PartialEq)]
+
+pub enum Rank {
+    Ace,
+    King,
+    Queen, 
+    Jack,
+    Number(u8),
+}
+    
+impl Suit {  
+    pub fn random() -> Suit {
+        let mut rng = rand::rng();
+        Suit::translate(rng.random_range(1..=4))
+    }
+
+    pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+            _ => unreachable!("Invalid suit value"),
+        }
+    }
+}
+impl Rank {
+    pub fn random() -> Rank {
+        let mut rng = rand::rng();
+        Rank::translate(rng.random_range(1..= 8))
+    }
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            2..=10 => Rank::Number(value),
+            _ => unreachable!("Invalid rank value"),
+        }
+    }
+}
+
+#[derive(Debug)]
+
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+pub fn winner_card(card: &Card) -> bool {
+    card.suit == Suit::Spade && card.rank == Rank::Ace
+}
+
+
