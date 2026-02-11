3c849838037ab9e3c5b976b3fc6b2e4008bbf270
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-11 02:17:36 +0100

    game tic tac toe

---
+++ tic_tac_toe/src/main.rs
@@ -0,0 +1,29 @@
+
+
+use tic_tac_toe::*;
+
+fn main() {
+    println!(
+        "{}",
+        tic_tac_toe([['O', 'X', 'O'], ['O', 'P', 'X'], ['X', '#', 'X']])
+    );
+    // tie
+    println!(
+        "{}",
+        tic_tac_toe([['X', 'O', 'O'], ['X', 'O', 'O'], ['#', 'O', 'X']])
+    );
+    // player O won
+
+    let diag = [['O', 'O', 'X'], ['O', 'X', 'O'], ['X', '#', 'X']];
+
+    println!("{}", tic_tac_toe(diag));
+    // player X won
+}
+
+// And its output
+
+// $ cargo run
+// tie
+// player O won
+// player X won
+// $
