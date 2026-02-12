46d1e4ab48b40aca680acac6736263dac95807ca
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-12 20:16:07 +0100

    edit distance

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -13,18 +13,20 @@ pub fn edit_distance(source: &str, target: &str) -> usize {
     for j in 0..=length_target {
         matrix[0][j] = j;
     }
-        for i in 1..= length_source {
-            for j in 1..= length_target {
-                let num = if vec_source[i-1] == vec_target[j -1] {0} else {1};               
-                matrix[i][j] = std::cmp::min (
-                    std::cmp::min(
-                    matrix[i-1][j] + 1,
-                    matrix[i][i-1] + 1
-                    ),
-                     matrix[i-1][j-1] + num
-                    );
-            };
-        }
-    matrix[length_source][length_target]
 
+    for i in 1..=length_source {
+        for j in 1..=length_target {
+            let cost = if vec_source[i - 1] == vec_target[j - 1] { 0 } else { 1 };
+
+            matrix[i][j] = std::cmp::min(
+                std::cmp::min(
+                    matrix[i - 1][j] + 1,     
+                    matrix[i][j - 1] + 1      
+                ),
+                matrix[i - 1][j - 1] + cost   
+            );
+        }
     }
+
+    matrix[length_source][length_target]
+}
