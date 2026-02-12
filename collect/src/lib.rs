96acfe7d0f48e1430e96e61aa5d6a98bd7a782a3
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-12 23:53:31 +0100

    ** sortables **

---
+++ collect/src/lib.rs
@@ -0,0 +1,13 @@
+
+pub fn bubble_sort(arr: &mut [i32]) {
+    let len = arr.len();
+    for i in 0..len  {
+        for j in 0..len - i - 1 {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
+
+
