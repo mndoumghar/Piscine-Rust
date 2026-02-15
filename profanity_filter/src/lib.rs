57ac784888b36af02b95f3fc890d421f7af6275c
Christopher  Fremond <dev@01-edu.org>
2026-02-15 17:50:26 +0000

    Update profanity_filter/src/lib.rs

--- profanity_filter/src/lib.rs
+++ profanity_filter/src/lib.rs
@@ -1,2 +1,17 @@
-// Empty file
+pub fn check_ms(word: &str) -> Result<&str, &str> {
+    if word == "" {
+        return Err("ERROR: illegal");
+    }
 
+    for wo in word.split_whitespace() {
+        if  wo =="hello"{
+            return Ok(wo);
+        }
+
+        if wo.contains("stupid") {
+            return Err("ERROR: illegal");
+        }
+    }
+
+    Ok(word)
+}
