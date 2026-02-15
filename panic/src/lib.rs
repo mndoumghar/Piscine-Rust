14f541641426ae24ae704f9f0b49111d86f0f58f
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-15 15:08:06 +0000

    painc()

---
+++ panic/src/lib.rs
@@ -0,0 +1,9 @@
+     
+     use std::fs::File;
+
+
+pub fn open_file(s: &str) -> File {
+
+    File::open(s).expect("")
+
+}
---
+++ panic/src/main.rs
@@ -0,0 +1,16 @@
+
+
+use panic::*;
+use std::fs::{self, File};
+
+fn main() {
+    let filename = "created.txt";
+    File::create(filename).unwrap();
+
+    println!("{:?}", open_file(filename));
+
+    fs::remove_file(filename).unwrap();
+
+    // this should panic!
+    open_file(filename);
+}
