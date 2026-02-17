c38172809dca24713b7d8f65bb31d725a7a0ef6a
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-17 12:35:06 +0000

    ordinal ..

---
+++ ordinal/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn num_to_ordinal(x: u32) -> String {
+    let res = match (x % 100) as u32 {
+        12 | 13 | 14 => "th",
+        _ =>
+            match( x % 10) as u32 {
+                1 => "st",
+                2 => "nd",
+                3 => "rd",
+                _ => "th",
+            }
+
+    };
+          x.to_string() + &res.to_string()
+}
---
+++ ordinal/src/main.rs
@@ -0,0 +1,9 @@
+use ordinal::*;
+
+fn main() {
+    println!("{}", num_to_ordinal(1));
+    println!("{}", num_to_ordinal(22));
+    println!("{}", num_to_ordinal(43));
+    println!("{}", num_to_ordinal(47));
+}
+
