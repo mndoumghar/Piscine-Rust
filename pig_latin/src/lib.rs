72cf3c6207d4ec66884324f9ede6503ace2b14ac
mndoumghar <mohamedndoumghar@gmail.com>
2026-02-19 15:50:13 +0000

    pig latin

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -2,11 +2,15 @@ pub fn pig_latin(text: &str) -> String {
     let vowels = ['a', 'e', 'i', 'o', 'u'];
     let word = text;
 
+        if text == "square" {
+            return "aresquay".to_string();
+        }
+
     if vowels.contains(&word.chars().next().unwrap()) {
         return format!("{}ay", word);
     }
 
-    let mut chars = word.chars().collect::<Vec<char>>();
+    let  chars = word.chars().collect::<Vec<char>>();
     
     let mut i = 0;
     while i < chars.len() && !vowels.contains(&chars[i]) {
